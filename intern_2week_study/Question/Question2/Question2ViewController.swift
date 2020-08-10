6f2140a6e42b627da2215892969607710429ad65
msugai-cq <m.sugai@caraquri.com>
2020-08-10 14:53:41 +0900

    add viewDidLoad

--- intern_2week_study/Question/Question2/Question2ViewController.swift
+++ intern_2week_study/Question/Question2/Question2ViewController.swift
@@ -5,6 +5,10 @@ final class Question2ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     private let areaTexts: [String] = ["茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県"]
+    
+    override func viewDidLoad() {
+        
+    }
 }
 
 extension Question2ViewController: UITableViewDataSource {
--- intern_2week_study/Question/Question3/Search/SearchViewController.swift
+++ intern_2week_study/Question/Question3/Search/SearchViewController.swift
@@ -2,4 +2,7 @@ import UIKit
 
 final class SearchViewController: UIViewController {
     
+    override func viewDidLoad() {
+        
+    }
 }
